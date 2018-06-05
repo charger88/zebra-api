7af0662d95dc0b7f3378e854daea0ef9b452ad0e
Michael Kelner <mikhail.kelner@gmail.com>
2018-06-04 22:34:42 -0400

    Remove ping log

--- controller_info.go
+++ controller_info.go
@@ -3,7 +3,6 @@ package main
 import (
 	"net/http"
 	"time"
-	"log"
 )
 
 type InfoPingResponse struct {
@@ -13,7 +12,6 @@ type InfoPingResponse struct {
 }
 
 func mInfoPing(r *http.Request, c Context) (int, JsonResponse, error) {
-	log.Print("Ping was called by " + r.UserAgent())
 	return 200, InfoPingResponse{time.Now().Unix(), true, testRedisConnection()}, nil
 }
 
